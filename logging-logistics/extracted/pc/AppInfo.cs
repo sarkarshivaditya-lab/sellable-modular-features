28602023ed9c628755944cd749ca361bec8682c4
Shivaditya Sarkar <sarkar.shivaditya@gmail.com>
2026-08-21 23:45:01 +0530

    Copy exact AppInfo.cs dependency for PC backup

---
+++ logging-logistics/extracted/pc/AppInfo.cs
@@ -0,0 +1,7 @@
+namespace UrbanDiagnosticCentre.Helpers;
+
+public static class AppInfo
+{
+    public const string Version = "1.0.0";
+    public const string AppName = "Urban Diagnostic Centre";
+}
