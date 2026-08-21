3f578ef5d74b22a6aabf9190310013bb2c07940e
Shivaditya Sarkar <sarkar.shivaditya@gmail.com>
2026-08-21 23:41:59 +0530

    Remove superseded snippet; use extracted verbatim source

--- multi-device-sync/src/Models/SyncCursor.cs
+++
@@ -1,9 +0,0 @@
-namespace UrbanDiagnosticCentre.Models;
-
-public class SyncCursor
-{
-    public int Id { get; set; }
-    public string EntityType { get; set; } = string.Empty;
-    public DateTime LastPulledAt { get; set; } = DateTime.MinValue;
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-}
